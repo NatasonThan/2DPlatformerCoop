471c1bce04e1eda1f80158ab1506d2fefdb94a9d
NatasonThan <natason.than@bumail.net>
2025-01-28 20:15:41 +0700

    PlayerMoveAndAnimation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [SerializeField] private float moveSpeed;
+
+    private float xInput;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    private void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        HandleAnimations();
+        HandleMovement();
+    }
+
+    private void HandleAnimations()
+    {
+        anim.SetBool("isRuning", rb.linearVelocityX != 0);
+    }
+
+    private void HandleMovement() 
+    {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocityY);
+    }
+}
